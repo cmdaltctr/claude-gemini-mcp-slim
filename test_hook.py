65948f7aaa351b4a24dab014a7e223a548c3f9ba
Aizat Hawari <aizat.hawari@gmail.com>
2025-07-11 05:07:25 +0800

    test: add test file for hook testing

---
+++ test_hook.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Simple test file for testing pre-edit hooks
+"""
+
+def hello_world():
+    """A simple function that greets the world"""
+    message = "Hello, World!"
+    print(message)
+    return "success"
+
+if __name__ == "__main__":
+    hello_world()
