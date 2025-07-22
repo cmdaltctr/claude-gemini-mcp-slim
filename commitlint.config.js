9250e6d72764859225b23a3a328c7d7f76713bce
Aizat Hawari <aizat.hawari@gmail.com>
2025-07-22 17:19:51 +0800

    ci: remove all commit message length restrictions for better developer experience

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,8 +20,9 @@ module.exports = {
     ],
     'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
-    'subject-max-length': [2, 'always', 250],
-    'header-max-length': [2, 'always', 250],
+    'subject-max-length': [0, 'always', Infinity],  // Disabled (0)
+    'header-max-length': [0, 'always', Infinity],   // Disabled (0)
+    'body-max-line-length': [0, 'always', Infinity], // Disabled (0)
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never']
   }
